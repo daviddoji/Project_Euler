8b9e477aff0d25b2ab2912f08aa6bbf426d04d92
daviddoji <daviddoji@protonmail.com>
2018-01-07 20:59:00 +0100

    Template for problems

---
+++ Python_solutions/Template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  7 20:56:26 2018
+
+@author: david
+
+Solution to Project Euler problem XXX
+
+https://github.com/daviddoji/Project_Euler
+"""
+
+
+def compute():
+    """
+
+    """
+
+
+if __name__ == "__main__":
+
+    print(compute())
